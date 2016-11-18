c5f53d06e927381fa5e909560a0574dfe8b92a03
Lou Huang <lou@louhuang.com>
2016-11-18 10:55:21 -0500

    polyfill fetch, move old js out of public

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+// Polyfills
+import 'whatwg-fetch'
+
+// React and React utilities
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Match } from 'react-router'
+
+// Application
 import App from './App'
 import './index.css'
 
